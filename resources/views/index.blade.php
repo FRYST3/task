69b3eaa0207ce189c342bc3a5638f8617ce63360
FRYST <bloodgrab317@yandex.by>
2024-08-11 21:07:03 +0300

    Верстка главной страницы

---
+++ resources/views/index.blade.php
@@ -0,0 +1,68 @@
+<!doctype html>
+<html lang="ru">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+
+    <link rel="stylesheet" href="/assets/css/style.css?v={{time()}}">
+
+    <script src="/assets/js/jquery.min.js"></script>
+    <script src="/assets/js/main.js"></script>
+
+    <title>BLOGNAME | FRYST</title>
+</head>
+<body>
+    <header class="header__container">
+        <a href="/" class="header_item">BLOGNAME</a>
+        <div class="header_nav">
+            <a href="/" class="header_nav__item">Главная</a>
+            <a href="/admin" class="header_nav__item">Администрирование</a>
+        </div>
+    </header>
+    <main class="main__container">
+        <div class="main_arcticles">
+            <div href="#" class="article_card">
+                <div class="article_card__img">
+                    <img src="/assets/images/articles.webp" alt="">
+                </div>
+                <h1 class="article_card__title">Blog test article</h1>
+                <div class="article_card_description">
+                    Blog test article Blog test article Blog test article Blog test article Blog test article Blog test article Blog test article Blog test article Blog test article Blog test article
+                </div>
+                <a href="#" class="article_card__btn">Читать</a>
+            </div>
+            <div href="#" class="article_card">
+                <div class="article_card__img">
+                    <img src="/assets/images/articles.webp" alt="">
+                </div>
+                <h1 class="article_card__title">Blog test article</h1>
+                <div class="article_card_description">
+                    Blog test article Blog test article Blog test article Blog test article Blog test article Blog test article Blog test article Blog test article Blog test article Blog test article
+                </div>
+                <a href="#" class="article_card__btn">Читать</a>
+            </div>
+            <div href="#" class="article_card">
+                <div class="article_card__img">
+                    <img src="/assets/images/articles.webp" alt="">
+                </div>
+                <h1 class="article_card__title">Blog test article</h1>
+                <div class="article_card_description">
+                    Blog test article Blog test article Blog test article Blog test article Blog test article Blog test article Blog test article Blog test article Blog test article Blog test article
+                </div>
+                <a href="#" class="article_card__btn">Читать</a>
+            </div>
+            <div href="#" class="article_card">
+                <div class="article_card__img">
+                    <img src="/assets/images/articles.webp" alt="">
+                </div>
+                <h1 class="article_card__title">Blog test article</h1>
+                <div class="article_card_description">
+                    Blog test article Blog test article Blog test article Blog test article Blog test article Blog test article Blog test article Blog test article Blog test article Blog test article
+                </div>
+                <a href="#" class="article_card__btn">Читать</a>
+            </div>
+        </div>
+    </main>
+</body>
+</html>
